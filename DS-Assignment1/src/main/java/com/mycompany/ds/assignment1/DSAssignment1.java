7b020a08889ed034b6c5ca7fc1567de37da4721c
b22chrmo <b22chrmo@student.his.se>
2024-09-25 09:12:17 +0200

    Add client part of assignment

---
+++ DS-Assignment1/src/main/java/com/mycompany/ds/assignment1/DSAssignment1.java
@@ -0,0 +1,24 @@
+package com.mycompany.ds.assignment1;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+public class DSAssignment1 {
+
+    public static void main(String[] args)
+    {
+        
+        QueueFrame queueFrame = new QueueFrame();
+        
+    }
+}
