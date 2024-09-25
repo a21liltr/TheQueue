1a1306e5df4b6e880cb673164aef254656fd2147
a21liltr <a21liltr@student.his.se>
2024-09-25 19:59:55 +0200

    Add PubSub functionality.

--- TheQueue.Server.Core/Services/ServerService.cs
+++ TheQueue.Server.Core/Services/ServerService.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 using NetMQ;
 using NetMQ.Sockets;
 using Newtonsoft.Json;
-using System.Text.RegularExpressions;
+using System.Text;
 using TheQueue.Server.Core.Models.BroadcastMessages;
 using TheQueue.Server.Core.Models.ClientMessages;
 using TheQueue.Server.Core.Models.ServerMessages;
@@ -16,9 +16,9 @@ namespace TheQueue.Server.Core.Services
 		private readonly IConfiguration _config;
 		private List<QueueTicket> _queue;
 
-		private bool serverIsRunning = true;
+		private bool _serverIsRunning = true;
 
-		private Queue<UserMessages> broadcastQueue; // type should be message
+		private Queue<string> _broadcastQueue; // type should be message
 
 		public ServerService(ILogger<ServerService> logger,
 			IConfiguration config)
@@ -27,20 +27,22 @@ namespace TheQueue.Server.Core.Services
 			_config = config;
 			_queue = new List<QueueTicket>();
 			var test = _config.GetValue<string>("test");
+			_broadcastQueue = new Queue<string>();
 		}
 
 		public void RunServer()
 		{
 			var port = 5555;
-            Task rrServer = Task.Run(() => { RunRequestReplyServer($"tcp://*:{port}"); });
-			//Task psServer = Task.Run(() => { RunPubSubServer($"tcp://*:{port+1}"); });
+			//Task rrServer = Task.Run(() => { RunRequestReplyServer($"tcp://*:{port}"); });
+			Task rrServer = Task.Run(() => { test(); });
+			Task psServer = Task.Run(() => { RunPubSubServer($"tcp://localhost:{port + 1}"); });
 
-			Task.WaitAll(rrServer);
+			Task.WaitAll(rrServer, psServer);
 		}
 
 		public void ShutdownServer()
 		{
-			serverIsRunning = false;
+			_serverIsRunning = false;
 		}
 
 		private void RunRequestReplyServer(string address)
@@ -49,7 +51,7 @@ namespace TheQueue.Server.Core.Services
 			{
 				responder.Bind(address); //port?
 				_logger.LogInformation("Server running");
-				while (serverIsRunning)
+				while (_serverIsRunning)
 				{
 					string message = responder.ReceiveFrameString();
 					_logger.LogInformation("Received message, {message}", message);
@@ -63,7 +65,7 @@ namespace TheQueue.Server.Core.Services
 
 					// TODO: Handle properly.
 
-					var received = (EnterQueueRequest) DeserializeJSON(message);
+					var received = (EnterQueueRequest)DeserializeJSON(message);
 					_logger.LogInformation($"Received message");
 
 					// kolla valid medddelande
@@ -82,28 +84,49 @@ namespace TheQueue.Server.Core.Services
 
 						responder.SendFrame(JsonConvert.SerializeObject(test));
 					}
+
+
+
+
+					Thread.Sleep(1000);
+
 				}
 			}
 		}
 
 		private void RunPubSubServer(string address)
 		{
-			using (var responder = new PublisherSocket())
+			using (var publisher = new PublisherSocket())
 			{
-				responder.Bind(address); //port?
-				while (serverIsRunning)
+				publisher.Bind(address); //port?
+				while (_serverIsRunning)
 				{
 					// peek at queue
 					// if any -> dequeue and broadcast
-					if (broadcastQueue.Peek() != null)
+					if (_broadcastQueue.Count is not 0)
 					{
-						var message = broadcastQueue.Dequeue();
-						responder.SendFrame($"Supervisor {message.Supervisor} announces {message.Message}");
+						var message = _broadcastQueue.Dequeue();
+						
+						publisher.SendFrame(message);
+						_logger.LogInformation("Published {msg}", message);
 					}
 
 					Thread.Sleep(1000);
 				}
 			}
+		}
+
+		private void test()
+		{
+			while (true)
+			{
+				_broadcastQueue.Enqueue("queue - test message");
+				_broadcastQueue.Enqueue("bla - bla message");
+				_logger.LogInformation("Enqueue");
+
+				Thread.Sleep(1000);
+			}
+
 		}
 
 		private object DeserializeJSON(string json)
