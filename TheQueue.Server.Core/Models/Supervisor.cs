69c33a6a35574a2bc03df20a809505d59cf1c0db
a21liltr <a21liltr@student.his.se>
2024-10-04 09:50:02 +0200

    Remove parent to class.

--- TheQueue.Server.Core/Models/Supervisor.cs
+++ TheQueue.Server.Core/Models/Supervisor.cs
@@ -1,11 +1,9 @@
-﻿using Newtonsoft.Json;
-using TheQueue.Server.Core.Enums;
-using TheQueue.Server.Core.Models.ClientMessages;
+﻿using TheQueue.Server.Core.Enums;
 using TheQueue.Server.Core.Models.ServerMessages;
 
 namespace TheQueue.Server.Core.Models
 {
-    public class Supervisor : RequestBase
+    public class Supervisor
     {
         public string Name { get; set; }
         public Status Status { get; set; }
