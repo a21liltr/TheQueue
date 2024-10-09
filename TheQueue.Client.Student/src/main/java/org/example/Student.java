1b9cf6f8308da37b2f7e3e8971da557efd173e1a
a21liltr <a21liltr@student.his.se>
2024-10-09 14:32:16 +0200

    Change Main file for student client to Student to be more clear.

--- TheQueue.Client.Student/src/main/java/org/example/Student.java
+++ TheQueue.Client.Student/src/main/java/org/example/Student.java
@@ -1,7 +1,7 @@
 package org.example;
 import UI.MainFrame;
 
-public class Main {
+public class Student {
     public static void main(String[] args) {
         System.out.println("Student Client started");
         new MainFrame(args, true);
